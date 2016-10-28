d2e13d6e6757a9a99db84f25ca58bf4c0ff16299
jayrave <jayanthan.raveendiran@gmail.com>
2016-10-27 22:41:28 -0700

    Add API to PristineModelsJsonAdapterFactory to allow lazy initialization of mappers

--- src/main/kotlin/com/jayrave/moshi/pristineModels/PristineModelsJsonAdapterFactory.kt
+++ src/main/kotlin/com/jayrave/moshi/pristineModels/PristineModelsJsonAdapterFactory.kt
@@ -6,7 +6,8 @@ import java.lang.reflect.Type
 import java.util.*
 
 class PristineModelsJsonAdapterFactory private constructor(
-        private val mappersForModels: Map<Class<*>, Mapper<*>>) :
+        private val mappersForModels: Map<Class<*>, Mapper<*>>,
+        private val mapperCreatorsForModels: Map<Class<*>, () -> Mapper<*>>) :
         JsonAdapter.Factory {
 
     /**
@@ -16,18 +17,64 @@ class PristineModelsJsonAdapterFactory private constructor(
             type: Type, annotations: MutableSet<out Annotation>?, moshi: Moshi):
             JsonAdapter<*>? {
 
-        return mappersForModels[type]?.buildJsonAdapter(moshi)
+        return mappersForModels[type]?.buildJsonAdapter(moshi) ?:
+                mapperCreatorsForModels[type]?.invoke()?.buildJsonAdapter(moshi)
     }
+
 
 
     class Builder {
 
-        private val mappersForModels = HashMap<Class<*>, Mapper<*>>()
+        private var mappersForModels: HashMap<Class<*>, Mapper<*>>? = null
+        private var mapperCreatorsForModels: HashMap<Class<*>, () -> Mapper<*>>? = null
 
-        fun build() = PristineModelsJsonAdapterFactory(mappersForModels)
+        /**
+         * Register a [mapper] to be used for a [clazz]. Calling this again for a [clazz] that
+         * has been registered will overwrite the existing mapper
+         */
         fun <T : Any> add(clazz: Class<T>, mapper: Mapper<T>): Builder {
-            mappersForModels[clazz] = mapper
+            if (mappersForModels == null) {
+                mappersForModels = HashMap()
+            }
+
+            mappersForModels!!.put(clazz, mapper)
             return this
+        }
+
+
+        /**
+         * Register a [mapperCreator] to be used to lazily create the mapper for a [clazz].
+         * Calling this again for a [clazz] that has been registered will overwrite the
+         * existing mapper
+         */
+        fun <T : Any> add(clazz: Class<T>, mapperCreator: () -> Mapper<T>): Builder {
+            if (mapperCreatorsForModels == null) {
+                mapperCreatorsForModels = HashMap()
+            }
+
+            mapperCreatorsForModels!!.put(clazz, mapperCreator)
+            return this
+        }
+
+
+        /**
+         * Builds a factory that provides json adapters for all the registered types
+         *
+         * *Note:* If both a mapper & a mapper creator are registered for the same class,
+         * the mapper has precedence over the mapper creator
+         */
+        fun build(): PristineModelsJsonAdapterFactory {
+            val mappers: Map<Class<*>, Mapper<*>> = when (mappersForModels) {
+                null -> emptyMap()
+                else -> HashMap(mappersForModels)
+            }
+
+            val mapperCreators: Map<Class<*>, () -> Mapper<*>> = when (mapperCreatorsForModels) {
+                null -> emptyMap()
+                else -> HashMap(mapperCreatorsForModels)
+            }
+
+            return PristineModelsJsonAdapterFactory(mappers, mapperCreators)
         }
     }
 }
--- src/test/kotlin/com/jayrave/moshi/pristineModels/PristineModelsJsonAdapterFactoryTest.kt
+++ src/test/kotlin/com/jayrave/moshi/pristineModels/PristineModelsJsonAdapterFactoryTest.kt
@@ -1,20 +1,15 @@
 package com.jayrave.moshi.pristineModels
 
 import com.squareup.moshi.Moshi
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class PristineModelsJsonAdapterFactoryTest {
 
     @Test
-    fun pristineModelsJsonAdapterFactoryWorks() {
-        class ExampleModel(val int: Int)
-        class ExampleModelMapper : Mapper<ExampleModel>() {
-            val int = field(ExampleModel::int, "int_field")
-            override fun create(value: Value<ExampleModel>): ExampleModel {
-                return ExampleModel(value of int)
-            }
-        }
+    fun withOnlyMappers() {
+        class ExampleModel()
+        class ExampleModelMapper : ThrowingMapper<ExampleModel>()
 
         val factory = PristineModelsJsonAdapterFactory.Builder()
                 .add(ExampleModel::class.java, ExampleModelMapper())
@@ -23,8 +18,84 @@ class PristineModelsJsonAdapterFactoryTest {
         // Use factory & make sure that the adapter built for the required model
         // is from the Mapper
         val moshi = Moshi.Builder().add(factory).build()
-        assertThat(moshi.adapter(ExampleModel::class.java).javaClass.canonicalName).isEqualTo(
-                "com.jayrave.moshi.pristineModels.Mapper.JsonAdapterForMapper"
-        )
+        assertJsonAdapterIsFromMapperFor(moshi, ExampleModel::class.java)
+    }
+
+
+    @Test
+    fun withOnlyMapperCreators() {
+        class ExampleModel()
+        class ExampleModelMapper : ThrowingMapper<ExampleModel>()
+
+        val factory = PristineModelsJsonAdapterFactory.Builder()
+                .add(ExampleModel::class.java, { ExampleModelMapper() })
+                .build()
+
+        // Use factory & make sure that the adapter built for the required model
+        // is from the Mapper
+        val moshi = Moshi.Builder().add(factory).build()
+        assertJsonAdapterIsFromMapperFor(moshi, ExampleModel::class.java)
+    }
+
+
+    @Test
+    fun mapperHasPrecedenceOverMapperCreator() {
+        class ExampleModel()
+        class ExampleModelMapper : ThrowingMapper<ExampleModel>()
+
+        var mapperCreatorInvoked = false
+        val mapperCreator = {
+            mapperCreatorInvoked = true
+            ExampleModelMapper()
+        }
+
+        val factory = PristineModelsJsonAdapterFactory.Builder()
+                .add(ExampleModel::class.java, mapperCreator)
+                .add(ExampleModel::class.java, ExampleModelMapper())
+                .build()
+
+        // Use factory & make sure that the adapter built for the required model
+        // is from the Mapper
+        val moshi = Moshi.Builder().add(factory).build()
+        assertThat(mapperCreatorInvoked).isFalse() // To make sure mapper creator wasn't involved
+        assertJsonAdapterIsFromMapperFor(moshi, ExampleModel::class.java)
+    }
+
+
+    @Test
+    fun withBothMappersAndMapperCreators() {
+        class ExampleModel1()
+        class ExampleModel2()
+        class ExampleModel1Mapper : ThrowingMapper<ExampleModel1>()
+        class ExampleModel2Mapper : ThrowingMapper<ExampleModel2>()
+
+        val factory = PristineModelsJsonAdapterFactory.Builder()
+                .add(ExampleModel1::class.java, ExampleModel1Mapper())
+                .add(ExampleModel2::class.java, { ExampleModel2Mapper() })
+                .build()
+
+        // Use factory & make sure that the adapter built for the required model
+        // is from the Mapper
+        val moshi = Moshi.Builder().add(factory).build()
+        assertJsonAdapterIsFromMapperFor(moshi, ExampleModel1::class.java)
+        assertJsonAdapterIsFromMapperFor(moshi, ExampleModel2::class.java)
+    }
+
+
+
+    private abstract class ThrowingMapper<T : Any> : Mapper<T>() {
+        override fun create(value: Value<T>): T {
+            throw UnsupportedOperationException("not implemented")
+        }
+    }
+
+
+
+    companion object {
+        private fun assertJsonAdapterIsFromMapperFor(moshi: Moshi, clazz: Class<*>) {
+            assertThat(moshi.adapter(clazz).javaClass.canonicalName).isEqualTo(
+                    "com.jayrave.moshi.pristineModels.Mapper.JsonAdapterForMapper"
+            )
+        }
     }
 }
