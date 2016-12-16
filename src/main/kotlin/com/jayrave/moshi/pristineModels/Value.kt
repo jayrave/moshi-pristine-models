374aa649b8f7246c56a7d6b94ee683263cb1245e
jayrave <jayanthan.raveendiran@gmail.com>
2016-12-16 10:13:06 -0800

    add `out` variance to Value's type parameter

--- src/main/kotlin/com/jayrave/moshi/pristineModels/Value.kt
+++ src/main/kotlin/com/jayrave/moshi/pristineModels/Value.kt
@@ -3,6 +3,6 @@ package com.jayrave.moshi.pristineModels
 /**
  * Extracts a value using the passed in [FieldMapping]
  */
-interface Value<T : Any> {
+interface Value<out T : Any> {
     infix fun <F> of(fieldMapping: FieldMapping<T, F>): F
 }
